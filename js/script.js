5e5f6897f624de82b70a348c9342028c6911e6ca
anashamimi <anashamimi2@gmail.com>
2023-03-30 01:24:51 +0200

    exercise icons

---
+++ js/script.js
@@ -0,0 +1,177 @@
+/*
+Milestone 1
+Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell'icona e l'icona stessa.
+Milestone 2
+Ciascuna icona ha una proprietà "color": utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
+Milestone 3
+Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone (animal, vegetable, user). Quando l'utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
+BONUS
+1- modificare la struttura dati fornita e valorizzare la proprietà "color" in modo dinamico: generare in modo casuale un codice colore, sapendo che la notazione esadecimale è formata dal simbolo "#" seguito da 6 caratteri alfanumerici compresi tra 0 e 9 e A e F.
+2- popolare le options della select della milestone 3 dinamicamente.
+*/
+
+const icons = [
+    {
+        name: 'cat',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'solid',
+        color: 'orange'
+    },
+    {
+        name: 'crow',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'solid',
+        color: 'orange'
+    },
+    {
+        name: 'dog',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'solid',
+        color: 'orange'
+    },
+    {
+        name: 'dove',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'solid',
+        color: 'orange'
+    },
+    {
+        name: 'dragon',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'solid',
+        color: 'orange'
+    },
+    {
+        name: 'horse',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'solid',
+        color: 'orange'
+    },
+    {
+        name: 'hippo',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'solid',
+        color: 'orange'
+    },
+    {
+        name: 'fish',
+        prefix: 'fa-',
+        type: 'animal',
+        family: 'solid',
+        color: 'orange'
+    },
+    {
+        name: 'carrot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'solid',
+        color: 'green'
+    },
+    {
+        name: 'apple-alt',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'solid',
+        color: 'green'
+    },
+    {
+        name: 'lemon',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'solid',
+        color: 'green'
+    },
+    {
+        name: 'pepper-hot',
+        prefix: 'fa-',
+        type: 'vegetable',
+        family: 'solid',
+        color: 'green'
+    },
+    {
+        name: 'user-astronaut',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'solid',
+        color: 'blue'
+    },
+    {
+        name: 'user-graduate',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'solid',
+        color: 'blue'
+    },
+    {
+        name: 'user-ninja',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'solid',
+        color: 'blue'
+    },
+    {
+        name: 'user-secret',
+        prefix: 'fa-',
+        type: 'user',
+        family: 'solid',
+        color: 'blue'
+    }
+];
+
+
+function cards(icon) {
+    const create = `
+    <div class="col-2 d-flex justify-content-center border rounded-4 icon-box shadow">
+        <div class="d-flex flex-column">
+            <i class="${icon.prefix}${icon.family} ${icon.prefix}${icon.name}" style="color: ${icon.color};"></i>
+            <span>${icon.name}</span>
+        </div>
+    </div>`;
+
+    return create
+}
+
+function drawPage(filteredIcons = icons) {
+    const container = document.querySelector('.row');
+    let html = '';
+    filteredIcons.forEach((icon) => {
+        const tpl = cards(icon);
+        html += tpl;
+    });
+    container.innerHTML = html;
+}
+
+function filterIcons() {
+    const select = document.getElementById('category');
+    const filteredArray = icons.filter((icon) => select.value === 'all' || icon.type === select.value);
+    drawPage(filteredArray);
+}
+
+function populateOptions(select) {
+    let optionValues = [];
+    icons.forEach((icon) => {
+        if(!optionValues.includes(icon.type)) {
+            optionValues.push(icon.type);
+            const newOption = document.createElement('option');
+            newOption.value = icon.type;
+            newOption.textContent = icon.type.charAt(0).toUpperCase() + icon.type.slice(1);
+            select.appendChild(newOption);
+        }
+    });
+}
+
+function init() {
+    const select = document.getElementById('category');
+    populateOptions(select);
+    select.addEventListener('change', filterIcons);
+    drawPage(icons);
+}
+
+init();
